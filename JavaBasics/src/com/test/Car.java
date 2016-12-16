74d5a11ed1d3376dc859350283e9b43888bca43e
Srikanth Gurram <2gurram@gmail.com>
2016-12-16 07:10:21 +0530

    Constructors and method overloading

--- JavaBasics/src/com/test/Car.java
+++ JavaBasics/src/com/test/Car.java
@@ -8,7 +8,63 @@ public class Car {
     String model;
     String make;
     String weight;
-    String color = "White";
+    String color;
+
+    //constructors are used to initialize the objects
+    //default constructor, executed by default
+    //method overloading:- methods with same name but different signature
+    // signature:- number of arguments, order of arguments, type of arguments
+
+    void message(){
+        System.out.println("default one");
+    }
+
+    void message(int i){
+        System.out.println("one integer as argument");
+    }
+
+    void message(String s){
+        System.out.println("one String as argument");
+    }
+
+    void message(String s1, String s2){
+        System.out.println("Two Strings as argument");
+    }
+
+    void message(String s, int i){
+        System.out.println("one string and one integer as argument");
+    }
+
+    void message(int i, String s){
+        System.out.println("one integer and one string as argument");
+    }
+
+
+
+    Car(){
+//        model = "AUDI X1";
+//        make = "AUDI Motors";
+        color = "Black";
+        weight = "6000";
+    }
+
+    Car(String carModel, String carColor, String carMake, String carWeight ){
+        model = carModel;
+        color = carColor;
+        make = carMake;
+        weight = carWeight;
+        //call the method after initialization of variables
+        getCarDetails();
+    }
+
+    Car(String carModel, String carColor, String carMake){
+        model = carModel;
+        color = carColor;
+        make = carMake;
+        weight = "1000";
+        //call the method after initialization of variables
+        getCarDetails();
+    }
 
     // methods
     void startCar(String carModel){
--- JavaBasics/src/com/test/UserOfCar.java
+++ JavaBasics/src/com/test/UserOfCar.java
@@ -6,29 +6,27 @@ public class UserOfCar {
     public static void main(String[] args){
 
         //Created an instance/object for Car class
-        Car fiatCar = new Car();
-        fiatCar.color = "Creamy white";
-        fiatCar.make = "Fiat";
-        fiatCar.weight = "5000";
-        fiatCar.model = "fiat punto";
-
-        //print the details of Fiat
-        fiatCar.getCarDetails();
+        Car fiatCar = new Car("Creamy white","Fiat","5000","fiat punto");
         fiatCar.startCar(fiatCar.model);
         fiatCar.stopCar();
         System.out.println("------------------------");
 
         // Create another object for the Car class
         Car audiCar = new Car();
-        audiCar.model = "AUDI X12";
-        audiCar.color = "Black";
-        audiCar.make = "AUDI Motors";
-        audiCar.weight = "6000";
 
         //print details of Audi
         audiCar.getCarDetails();
         audiCar.startCar(audiCar.model);
         audiCar.stopCar();
 
+        //create another object for car class
+        Car tataCar = new Car("Nano","White","M&M Motors");
+
+        tataCar.message();
+        tataCar.message(100);
+        tataCar.message("Hello");
+        tataCar.message("Hello", 100);
+        tataCar.message(100, "Hello");
+        tataCar.message("Helo", "How are you?");
     }
 }
